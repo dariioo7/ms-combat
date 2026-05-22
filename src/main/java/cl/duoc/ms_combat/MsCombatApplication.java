fecdb102e3a08c6a168c05a7133a6a882fad4038
Dario Sagredo <dariosagredo@gmail.com>
2026-05-21 22:02:05 -0400

    Microservicio combat creado con dependencias

---
+++ src/main/java/cl/duoc/ms_combat/MsCombatApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_combat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCombatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCombatApplication.class, args);
+	}
+
+}
